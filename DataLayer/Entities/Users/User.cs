46829002e79d60c7edeb0b2ec3cea953b90b1489
Samenjad92 <m.samnejad92@gmail.com>
2023-11-18 07:31:53 +0330

    Add project files.

---
+++ DataLayer/Entities/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities.Users
+{
+    class User:BaseEntity
+    {
+        [MaxLength(50)]
+        public string UserName { get; set; }
+        [MinLength(6)]
+        [MaxLength(50)]
+        public string Password { get; set; }
+        public string Avatar { get; set; }
+    }
+}
